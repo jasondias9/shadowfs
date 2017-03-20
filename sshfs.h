0bb520e8c0d812972a8c991612c55bb6e4f2d67f
Jason Dias <dias.jason9@gmail.com>
2017-03-20 19:55:51 -0400

    Updated ssfs header

--- sshfs.h
+++ sshfs.h
@@ -2,7 +2,8 @@
  * @name        mkssfs
  * @param       fresh      If 1, create the file system from scratch. 
  *                         Otherwise the file system is opened from disk
- * Creates a new file system
+ * Formats the virtual disk implemented by the disk emulator and creates an instance
+ * of SSFS 
  *
 */
 void mkssfs(int fresh);
@@ -10,8 +11,12 @@ void mkssfs(int fresh);
 /*!
  * @name        ssfs_fopen
  * @param       name        Name of the file system
+ * @return      integer corresponding to the index of the entry in the file
+ *              descriptor table
  *
- * Opens the file system
+ * Opens the file in append mode (sets write file pointer to the end of the file, 
+ * sets read file pointer to the beginning of the file. If the file does not exist, 
+ * create a new file and sets size to 0. 
 */
 int ssfs_fopen(char *name);
 
@@ -19,7 +24,7 @@ int ssfs_fopen(char *name);
  * @name        ssfs_fclose
  * @param       fileID       ID of the file system
  *
- * Closes the given file system
+ * Close a file, i.e. removes the entry from the file descriptor table
 */
 int ssfs_fclose(int fileID);
 
@@ -27,8 +32,9 @@ int ssfs_fclose(int fileID);
  * @name         ssfs_frseek
  * @param        fileID       ID of the file system
  * @param        loc          location to seek until
- *
- * Seek (read) to the location from the beginning
+ * @return       On success return 1, negative number otherwise.
+ * 
+ * Moves the read pointer to the location specified
 */
 int ssfs_frseek(int fileID, int loc);
 
@@ -36,8 +42,9 @@ int ssfs_frseek(int fileID, int loc);
  * @name         ssfs_fwseek
  * @param        fileID       ID of the file system
  * @param        loc          location to seek until
+ * @return       On success return 1, negative number otherwise.
  * 
- * Seek (write) to the location from the beginning
+ * Moves the write pointer to the location specified.
 */
 int ssfs_fwseek(int fileID, int loc);
 
@@ -46,8 +53,11 @@ int ssfs_fwseek(int fileID, int loc);
  * @param         fileID       ID of the file system
  * @param         buf          pointer to character stream to be written to disk
  * @param         length       length of the character stream
+ * @return        On success the number of bytes written to the file system,
+ *                else a negative number
  *
- * Write buffer characters onto the disk
+ * Writes the given number of bytes in buf into the open file, starting from the 
+ * current write file pointer.
 */
 int ssfs_fwrite(int fileID, char *buf, int length);
 
@@ -57,8 +67,10 @@ int ssfs_fwrite(int fileID, char *buf, int length);
  * @param         fileID       ID of the file system
  * @param         buf          pointer to character stream to be read into from disk
  * @param         length       length of the data to be read from the disk
+ * @return        On success the number of bytes written to buf, else a negative number
  *
- * Read characters from disk into buffer
+ * Reads the given number of bytes into buf from the open file, starting from the 
+ * current read file pointer.
 */
 int ssfs_fread(int fileID, chr *buf, int length);
 
